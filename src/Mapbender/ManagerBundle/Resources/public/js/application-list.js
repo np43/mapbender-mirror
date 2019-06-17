a8c5ecbf0d843910fdf9646fafe2033f34642203
David Patzke <davidpatzke@gmx.de>
2019-06-17 06:43:48 +0200

    Reimplement list filter

--- src/Mapbender/ManagerBundle/Resources/public/js/application-list.js
+++ src/Mapbender/ManagerBundle/Resources/public/js/application-list.js
@@ -1,33 +1,28 @@
-$(function(){
+$(function () {
     // Switch application state via Ajax when the current state icon is clicked
-    $('#listFilterApplications .iconPublish[data-url]').bind('click', function() {
+    $('.-fn-application .iconPublish[data-url]').bind('click', function () {
         var $this = $(this);
         var url = $this.attr('data-url');
-        var requestedState;
+        var requestedState = $this.hasClass("enabled") ? "disabled" : "enabled";;
 
-        if($this.hasClass("enabled")){
-            requestedState = "disabled";
-        }else{
-            requestedState = "enabled";
-        }
+
 
         $.ajax({
             url: url,
             type: 'POST',
-            data: {state: requestedState}
-        }).done(function(response) {
-            if (response.newState === 'enabled') {
-                $this.removeClass("disabled").addClass('enabled iconPublishActive');
-            } else {
-                $this.removeClass('enabled iconPublishActive').addClass('disabled');
-            }
-        }).fail(function(jqXHR, textStatus, errorThrown) {
+            data: { state: requestedState }
+        }).done(function (response) {
+            var isEnabled = response.newState === 'enabled'
+            isEnabled ? $this.removeClass("disabled").addClass('enabled iconPublishActive') : $this.removeClass('enabled iconPublishActive').addClass('disabled');
+
+
+        }).fail(function (jqXHR, textStatus, errorThrown) {
             Mapbender.error(errorThrown);
         });
         return false;
     });
 
-    $('#listFilterApplications').on('click', '.iconRemove[data-url]', function() {
+    $('#listFilterApplications').on('click', '.iconRemove[data-url]', function () {
         var $el = $(this);
         Mapbender.Manager.confirmDelete($el, $el.attr('data-url'), {
             // @todo: bring your own translation string
@@ -39,4 +34,16 @@ $(function(){
         });
         return false;
     });
+
+
+    $('.-fn-list-search').keypress(function (event) {
+        var searchValue = $(event.currentTarget).val();
+        $('.-fn-application').prop('hidden', false);
+        $('.-fn-application').each(function (iterator, node) {
+            var iTitle = $(node).find(".-fn-application-title").text();
+            if (!(iTitle.match(new RegExp(".*" + searchValue + ".*", "i")))) {
+                $(node).prop('hidden', true)
+            }
+        })
+    })
 });
